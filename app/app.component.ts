539b4098137a911c28a9c40ea02e0958c23b68a1
Guillermo Jose Rodriguez Fernandez <grodrigu@macl2004.eu.acncorp.com>
2016-03-31 10:15:42 +0200

    Adding list of beers

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,46 @@
 import {Component} from 'angular2/core';
 
+export class Beer {
+  id: number;
+  name: string;
+}
+
 @Component({
+
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>{{title}}</h1>
+    		   <ul class="beers">
+  			   <li *ngFor="#beer of beers">
+  					<span class="badge">{{beer.id}}</span> {{beer.name}}
+				</li>
+			   </ul>
+    		   <h2>{{beer.name}} details!</h2>
+    		   <div><label>id: </label>{{beer.id}}</div>
+    		   <div>
+    		   		<label>name: </label>{{beer.name}}
+					<input [(ngModel)]="beer.name" placeholder="name">
+    		   </div> `
 })
-export class AppComponent { }
+
+export class AppComponent { 
+
+  beers = BEERS;
+  title = 'List of Recipes';
+  beer: Beer = {
+  	id:1,
+  	name: 'IPA'
+  }
+}
+
+var BEERS: Beer[] = [
+  { "id": 11, "name": "Red Imperial Ale" },
+  { "id": 12, "name": "Pale Ale" },
+  { "id": 13, "name": "White Beer" },
+  { "id": 14, "name": "Extra Stout" },
+  { "id": 15, "name": "Bock" },
+  { "id": 16, "name": "Dubbel" },
+  { "id": 17, "name": "Tripel" },
+  { "id": 18, "name": "Pilsner" },
+  { "id": 19, "name": "ScotchAle" },
+  { "id": 20, "name": "IrishAle" }
+];
